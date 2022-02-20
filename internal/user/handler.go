145c59487da42a163f4da5a9376d3a37ee19b22a
ellywynn <canttrymybest@gmail.com>
2022-02-20 15:42:38 +0300

    implemented user handlers

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	usersURL = "/api/users"
-	userURL  = "/api/users/:uuid"
+	userURL  = "/api/users/:id"
 )
 
 // Handler handles requests specified to user service.
@@ -41,32 +41,29 @@ func (h *Handler) Register(router *httprouter.Router) {
 	router.HandlerFunc(http.MethodDelete, userURL, h.DeleteUser)
 }
 
-// GetUser godoc
-// @Summary Show user information
-// @Description Get user by uuid.
-// @Tags users
-// @Accept json
-// @Produce json
-// @Param uuid path string true "User id"
-// @Success 200 {object} User
-// @Failure 404 {object} apperror.AppError
-// @Failure 500 {object} apperror.AppError
-// @Router /users/{uuid} [get]
 func (h *Handler) GetUser(w http.ResponseWriter, r *http.Request) {
 	h.logger.Info("GET USER")
+
+	id, err := handler.ReadIdParam64(r)
+	if err != nil {
+		response.BadRequest(w, err.Error(), "")
+		return
+	}
+
+	user, err := h.userService.GetById(r.Context(), id)
+	if err != nil {
+		if errors.Is(err, apperror.ErrNoRows) {
+			response.NotFound(w)
+			return
+		}
+		h.logger.Error(err)
+		response.InternalError(w, err.Error(), "")
+		return
+	}
+
+	response.JSON(w, http.StatusOK, user)
 }
 
-// CreateUser godoc
-// @Summary Create user
-// @Description Register a new user.
-// @Tags users
-// @Accept json
-// @Produce json
-// @Param input body user.CreateUserDTO true "JSON input"
-// @Success 201 {object} internal.CreateUserResponse
-// @Failure 400 {object} apperror.AppError
-// @Failure 500 {object} apperror.AppError
-// @Router /users [post]
 func (h *Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	h.logger.Info("CREATE USER")
 
@@ -99,55 +96,124 @@ func (h *Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	response.JSON(w, http.StatusCreated, user)
 }
 
-// GetUserByEmailAndPassword godoc
-// @Summary Get user by email and password from query parameters
-// @Description Create a new user
-// @Tags users
-// @Accept json
-// @Produce json
-// @Param email query string true "user email"
-// @Param password query string true "user raw password"
-// @Success 200 {object} User
-// @Failure 400 {object} apperror.AppError
-// @Failure 404 {object} apperror.AppError
-// @Failure 500 {object} apperror.AppError
-// @Router /users [get]
 func (h *Handler) GetUserByEmailAndPassword(w http.ResponseWriter, r *http.Request) {
 	h.logger.Info("GET USER BY EMAIL AND PASSWORD")
+
+	email := r.URL.Query().Get("email")
+	password := r.URL.Query().Get("password")
+
+	if email == "" || password == "" {
+		response.BadRequest(w, "empty email or password", "email and password must be provided")
+		return
+	}
+
+	user, err := h.userService.GetByEmailAndPassword(r.Context(), email, password)
+	if err != nil {
+		if errors.Is(err, apperror.ErrNoRows) {
+			response.NotFound(w)
+			return
+		}
+		response.BadRequest(w, err.Error(), "")
+		return
+	}
+
+	response.JSON(w, http.StatusOK, user)
 }
 
 func (h *Handler) UpdateUser(w http.ResponseWriter, r *http.Request) {
 	h.logger.Info("UPDATE USER")
+
+	id, err := handler.ReadIdParam64(r)
+	if err != nil {
+		response.BadRequest(w, err.Error(), "")
+		return
+	}
+
+	var input UpdateUserDTO
+	if err := response.ReadJSON(w, r, &input); err != nil {
+		response.BadRequest(w, err.Error(), "please, fix your request body")
+		return
+	}
+
+	if err := input.Validate(); err != nil {
+		response.BadRequest(w, err.Error(), "")
+		return
+	}
+
+	input.Id = id
+
+	err = h.userService.Update(r.Context(), &input)
+	if err != nil {
+		switch err {
+		case apperror.ErrNoRows:
+			response.NotFound(w)
+		case apperror.ErrWrongPassword:
+			response.BadRequest(w, err.Error(), "")
+		default:
+			response.InternalError(w, err.Error(), "")
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
-// UpdateUserPartially godoc
-// @Summary Update user
-// @Description Partially update the user with provided current password.
-// @Tags users
-// @Accept json
-// @Produce json
-// @Param uuid path string true "User id"
-// @Param input body user.UpdateUserDTO true "JSON input"
-// @Success 200
-// @Failure 400 {object} apperror.AppError
-// @Failure 404 {object} apperror.AppError
-// @Failure 500 {object} apperror.AppError
-// @Router /users/{uuid} [patch]
 func (h *Handler) UpdateUserPartially(w http.ResponseWriter, r *http.Request) {
 	h.logger.Info("UPDATE USER PARTIALLY")
+
+	id, err := handler.ReadIdParam64(r)
+	if err != nil {
+		response.BadRequest(w, err.Error(), "")
+		return
+	}
+
+	var input UpdateUserPartiallyDTO
+	if err := response.ReadJSON(w, r, &input); err != nil {
+		response.BadRequest(w, err.Error(), "please, fix your request body")
+		return
+	}
+
+	if err := input.Validate(); err != nil {
+		response.BadRequest(w, err.Error(), "")
+		return
+	}
+
+	input.Id = id
+
+	err = h.userService.UpdatePartially(r.Context(), &input)
+	if err != nil {
+		switch err {
+		case apperror.ErrNoRows:
+			response.NotFound(w)
+		case apperror.ErrWrongPassword:
+			response.BadRequest(w, err.Error(), "")
+		default:
+			response.InternalError(w, err.Error(), "")
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
-// DeleteUser godoc
-// @Summary Delete user
-// @Description Delete the user by uuid.
-// @Tags users
-// @Accept json
-// @Produce json
-// @Param uuid path string true "User id"
-// @Success 200
-// @Failure 404 {object} apperror.AppError
-// @Failure 500 {object} apperror.AppError
-// @Router /users/{uuid} [delete]
 func (h *Handler) DeleteUser(w http.ResponseWriter, r *http.Request) {
 	h.logger.Info("DELETE USER")
+
+	id, err := handler.ReadIdParam64(r)
+	if err != nil {
+		response.BadRequest(w, err.Error(), "")
+		return
+	}
+
+	err = h.userService.Delete(r.Context(), id)
+	if err != nil {
+		if errors.Is(err, apperror.ErrNoRows) {
+			response.NotFound(w)
+			return
+		}
+		response.InternalError(w, err.Error(), "something went wrong on the server side")
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
