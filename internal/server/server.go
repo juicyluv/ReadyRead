75041652c303c574456849826e087b2e3abbeca8
ellywynn <canttrymybest@gmail.com>
2022-02-20 18:05:34 +0300

    initializing handlers inside server.Run()

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,15 +5,21 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/jackc/pgx/v4"
 	"github.com/juicyluv/ReadyRead/config"
+	"github.com/juicyluv/ReadyRead/internal/author"
+	"github.com/juicyluv/ReadyRead/internal/openapi"
+	"github.com/juicyluv/ReadyRead/internal/user"
 	"github.com/juicyluv/ReadyRead/pkg/logger"
 	"github.com/julienschmidt/httprouter"
 )
 
 // Server represents http server.
 type Server struct {
-	server *http.Server
-	logger *logger.Logger
+	server  *http.Server
+	logger  *logger.Logger
+	cfg     *config.Config
+	handler *httprouter.Router
 }
 
 // NewServer returns a new Server instance.
@@ -26,17 +32,38 @@ func NewServer(cfg *config.Config, handler *httprouter.Router, logger *logger.Lo
 			MaxHeaderBytes: cfg.Http.MaxHeaderBytes << 20,
 			Addr:           ":" + cfg.Http.Port,
 		},
-		logger: logger,
+		logger:  logger,
+		cfg:     cfg,
+		handler: handler,
 	}
 }
 
-// Run starts http server. Returns an error on failure.
-func (s *Server) Run() error {
+// Run initializes storages, services, handlers and then starts http server. Returns an error on failure.
+func (s *Server) Run(dbConn *pgx.Conn) error {
+	reqTimeout := s.cfg.DB.RequestTimeout
+
+	s.logger.Info("initializing routes")
+
+	userStorage := user.NewStorage(dbConn, reqTimeout)
+	userService := user.NewService(userStorage, *s.logger)
+	userHandler := user.NewHandler(*s.logger, userService)
+	userHandler.Register(s.handler)
+	s.logger.Info("initialized user routes")
+
+	authorStorage := author.NewStorage(dbConn, reqTimeout)
+	authorService := author.NewService(authorStorage, *s.logger)
+	authorHandler := author.NewHandler(*s.logger, authorService)
+	authorHandler.Register(s.handler)
+	s.logger.Info("initialized author routes")
+
+	openapi.InitSwagger(s.handler)
+	s.logger.Info("initialized documentation")
+
 	return s.server.ListenAndServe()
 }
 
 // Shutdown closes all connections and shuts down http server.
-// It uses httpServer.Shutdown() function. Returns an error on failure.
+// It uses httpServer.Shutdown() method. Returns an error on failure.
 func (s *Server) Shutdown(ctx context.Context) error {
 	return s.server.Shutdown(ctx)
 }
